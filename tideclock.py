bb5838380719a65d3600978409cb835b3cbe532a
Quentin Barbarat <quentin.barbarat@epita.fr>
2020-10-17 14:35:03 +0200

    tideclock.py: add data fetching methods

--- tideclock.py
+++ tideclock.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python3
 
 import argparse
+import csv
 import logging
 import sys
 import time
+from datetime import date
 
 import display
+
+
+def getRowFromCSV(filename, rowNum):
+    """Get a specific row from a given file"""
+
+    with open(filename) as file:
+
+        reader = csv.reader(file)
+
+        rows = [row for ndx, row in enumerate(reader) if ndx == rowNum]
+
+    return rows[0]
+
+
+def getDataForToday():
+    """Get TideData for today's date"""
+
+    today = date.today()
+
+    ndx = today.toordinal() - date(today.year, 1, 1).toordinal() + 1
+    row = getRowFromCSV(f"data/{today.year}.csv", ndx)
+
+    # Create Data
+
+    return row
+
 
 if __name__ == "__main__":
 
@@ -28,13 +56,17 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.dry_run:
-        sys.exit()
-
     loglevel = logging.ERROR if args.mode == "production" else logging.DEBUG
 
     logging.basicConfig(level=loglevel)
     logging.info("Starting tideclock")
+
+    data = getDataForToday()
+
+    if args.dry_run:
+
+        print(data)
+        sys.exit()
 
     display = display.Display()
 
